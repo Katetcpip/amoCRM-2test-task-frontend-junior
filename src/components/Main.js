72f85c767e044e0060f831652231e47860d72c8a
Katetcpip <katetcpip@yabdex.ru>
2023-05-07 20:27:07 +0300

    renamed

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,12 +10,12 @@ function Main() {
         <
         h1 className = "hero__title" >
         Зарабатывайте больше <
-        span className = "hero__title_accent" > с WELBEX < /span> <
-        /h1>
+        span className = "hero__title_accent" > с WELBEX < /span> < /
+        h1 >
 
         <
-        p className = "hero__text" > Развиваем и контролируем продажи за вас < /p> <
-        /div> <
+        p className = "hero__text" > Развиваем и контролируем продажи за вас < /p> < /
+        div > <
         div className = "hero__about" >
         <
         h2 className = "hero__about-title" >
@@ -31,25 +31,25 @@ function Main() {
         li >
         <
         h3 className = "hero__feature-title" > Виджеты < /h3> <
-        p className = "hero__feature-text" > 30  готовых решений < /p> <
-        /li> <
+        p className = "hero__feature-text" > 30  готовых решений < /p> < /
+        li > <
         li >
         <
         h3 className = "hero__feature-title" > Dashboard < /h3> <
         p className = "hero__feature-text" >
         с  показателями вашего бизнеса <
-        /p> <
-        /li> <
+        /p> < /
+        li > <
         li >
         <
         h3 className = "hero__feature-title" > Skype Аудит < /h3> <
-        p className = "hero__feature-text" > отдела продаж и  CRM системы < /p> <
-        /li> <
+        p className = "hero__feature-text" > отдела продаж и  CRM системы < /p> < /
+        li > <
         li >
         <
         h3 className = "hero__feature-title" > 35  дней < /h3> <
-        p className = "hero__feature-text" > использования CRM < /p> <
-        /li> <
+        p className = "hero__feature-text" > использования CRM < /p> < /
+        li > <
         /ul> <
         ul className = "hero__features_mobile" >
         <
@@ -57,8 +57,8 @@ function Main() {
         <
         p className = "hero__feature-text" >
         <
-        span className = "hero__feature_accent" > — < /span> Skype Аудит <
-        /p> <
+        span className = "hero__feature_accent" > — < /span> Skype Аудит < /
+        p > <
         /li>
 
         <
@@ -66,28 +66,28 @@ function Main() {
         <
         p className = "hero__feature-text" >
         <
-        span className = "hero__feature_accent" > — < /span> 30 виджетов <
-        /p> <
+        span className = "hero__feature_accent" > — < /span> 30 виджетов < /
+        p > <
         /li> <
         li >
         <
         p className = "hero__feature-text" >
         <
-        span className = "hero__feature_accent" > — < /span> Dashboard <
-        /p> <
+        span className = "hero__feature_accent" > — < /span> Dashboard < /
+        p > <
         /li> <
         li >
         <
         p className = "hero__feature-text" >
         <
-        span className = "hero__feature_accent" > — < /span> Месяц AmoCRM <
-        /p> <
-        /li> <
-        /ul> <
-        button className = "hero__about-button" > Получить консультацию < /button> <
-        /div> <
-        /section> <
-        /main>
+        span className = "hero__feature_accent" > — < /span> Месяц AmoCRM < /
+        p > <
+        /li> < /
+        ul > <
+        button className = "hero__about-button" > Получить консультацию < /button> < /
+        div > <
+        /section> < /
+        main >
     );
 }
 
